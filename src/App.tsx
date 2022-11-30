ce410c770deaaadb1c85d55273b20301acea09b8
partha108paul <102946785+partha108paul@users.noreply.github.com>
2022-12-01 02:55:26 +0530

    first commit2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { Button, Flex } from "@chakra-ui/react";
+import Cookies from "js-cookie";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState<any>();
+  const ViewResult = async () => {
+    console.log("Token", data && data.result.token);
+    Cookies.set("token", data?.result?.token);
+  };
+
+  const RandomColor = async () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      flag: false,
+      sessionID: "9d1cff3e-7f6f-437c-9a57-c94e2655151e",
+      otp: "21470",
+      phone: "+913698521470",
+    });
+
+    const response = await fetch("http://127.0.0.1:17001/v1/auth/login", {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    })
+      .then((r) => r.json())
+      .catch((error) => console.log("error", error));
+    setData(response);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex h="100vh" justifyContent="center" alignItems="center">
+      <Button
+        /* flex={1} */
+        px={4}
+        fontSize={"sm"}
+        rounded={"full"}
+        bg={"blue.400"}
+        color={"white"}
+        boxShadow={
+          "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+        }
+        _hover={{
+          bg: "blue.500",
+        }}
+        _focus={{
+          bg: "blue.500",
+        }}
+        onClick={() => RandomColor()}
+      >
+        Sign In
+      </Button>
+      <Button
+        /* flex={1} */
+        px={4}
+        ml={10}
+        fontSize={"sm"}
+        rounded={"full"}
+        bg={"blue.400"}
+        color={"white"}
+        boxShadow={
+          "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+        }
+        _hover={{
+          bg: "blue.500",
+        }}
+        _focus={{
+          bg: "blue.500",
+        }}
+        onClick={() => ViewResult()}
+      >
+        SSO Login
+      </Button>
+    </Flex>
   );
-}
+};
 
 export default App;
